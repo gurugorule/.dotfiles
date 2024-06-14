bdafc1b558f057a35b3608b46446908d06145744
Gurunath Gorule <goruleguru@gmail.com>
2024-06-14 23:37:18 +0530

    modified:   setup.sh         added a error function if dependencies not installed

--- setup.sh
+++ setup.sh
@@ -7,15 +7,26 @@ if [ "$(id -u)" -eq 0 ]; then
 # only for Debian/Ubuntu installing dependencies
   if [ -e /etc/os-release ]; then
     grep -q "ubuntu" /etc/os-release
-    echo "apt pkg updating... and installing dependencies.."
+    echo "apt pkg updating..."
     sleep 0.5
     apt update
-    apt install -y git 
   fi
 else 
   error "This script requires superuser permissions. Please re-run as root."
   exit 1
 fi
+
+# function to check if command found. 
+check_command() {
+    if ! command -v "$1" >/dev/null 2>&1; then
+      echo "$1 is not installed :) first install dependencies.. then re-run the script  Exiting."
+      exit 1
+    fi
+}
+
+# Check if tmux and neovim are installed
+check_command tmux
+check_command nvim
 
 file_one=~/.dotfiles/.config/nvim/chadrc.lua
 file_two=~/.config/nvim/lua/chadrc.lua
