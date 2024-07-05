1361d8255db571cae5be33952773dd349a803d13
Gurunath Gorule <goruleguru@gmail.com>
2024-07-05 22:37:49 +0530

    wrap-up everything into function

--- setup.sh
+++ setup.sh
@@ -4,25 +4,27 @@
 error() { echo -e "ERROR \n$*"; exit 1; }
 
 # Ping Google's public DNS server and Check if device is connected to Internet.
-ping -c 1 8.8.8.8 &> /dev/null
-if [ $? -ne 0 ]; then
-    error "No Internet Connection. \nPlease Connect to Internet"
-    exit 1
-fi
+check_connection(){
+  ping -c 1 8.8.8.8 &> /dev/null
+  if [ $? -ne 0 ]; then
+      error "No Internet Connection. \nPlease Connect to Internet"
+      exit 1
+  fi
+}
 
-# check if superuser
-if [ "$(id -u)" -eq 0 ]; then
-# only for Debian/Ubuntu installing dependencies
-  if [ -e /etc/os-release ]; then
-    grep -q "ubuntu" /etc/os-release
+# Update Apt Pkg Repo
+update_pkg_repo(){
+  if [ "$(id -u)" -eq 0 ]; then
     echo "apt pkg updating..."
     sleep 0.5
     apt update
+  else 
+    error "This script requires superuser permissions. \nPlease re-run as root."
+    exit 1
   fi
-else 
-  error "This script requires superuser permissions. \nPlease re-run as root."
-  exit 1
-fi
+
+}
+
 
 # function to check if command found. 
 check_command() {
@@ -32,7 +34,10 @@ check_command() {
     fi
 }
 
-# Check if tmux and neovim are installed
+check_connection
+
+update_pkg_repo
+
 check_command tmux
 check_command nvim
 check_command kitty
